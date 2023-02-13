12406b6d3055fd6148e123f08d348ee8c9ea999a
Towhid <towhidgolam8182@gmail.com>
2023-02-13 22:35:57 +0600

    Binary search

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const arr = [1, 2, 3, [41, [421, 422, 423, 424], 43], [51, 52], 6];
+/* const arr = [1, 2, 3, [41, [421, 422, 423, 424], 43], [51, 52], 6];
 
 function rev(test) {
   let arr2 = []; // new fresh array for output
@@ -16,4 +16,23 @@ function rev(test) {
   return arr2;
 }
 
-console.log(rev(arr));
+console.log(rev(arr)); */
+
+const arr = [-1, 0, 3, 5, 9, 12];
+const target = 0;
+
+function findIndex(array) {
+  const index = Math.floor(array.length / 2);
+
+  if (array[index] == target) {
+    return index;
+  } else if (array[index] < target) {
+    return index + 1 + findIndex(array.slice(index + 1));
+  } else if (array[index] > target) {
+    return findIndex(array.slice(0, index));
+  } else {
+    return undefined;
+  }
+}
+
+console.log(findIndex(arr) || -1);
